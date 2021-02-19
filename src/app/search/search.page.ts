e3952c1cabdccad1747b13a23686ef4004dcd5c3
Tomas-a21803655 <57908569+Tomas-a21803655@users.noreply.github.com>
2021-02-19 18:48:04 +0000

    fixed get all trips im a boss

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {AngularFirestore} from '@angular/fire/firestore';
-import {Observable} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 import {HomeTripCardsModel} from '../shared/homeTripCards.model';
 
 @Component({
@@ -10,19 +10,27 @@ import {HomeTripCardsModel} from '../shared/homeTripCards.model';
   styleUrls: ['./search.page.scss'],
 })
 export class SearchPage implements OnInit {
-  public homeTripCards: Observable<Array<HomeTripCardsModel>>;
+  public allHomeTripCards: any = [];
 
   constructor(private router: Router, public db: AngularFirestore) {
   }
 
   ngOnInit() {
-    this.getAllhomeTripCards().subscribe((data) => {
-      this.homeTripCards = data;
-      console.log(data);
-    });
+    this.db.collection('users').get()
+        .subscribe(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            this.getTargetUserTrips(doc.id);
+          });
+        });
   }
 
-  getAllhomeTripCards(): Observable<any> {
-    return this.db.collection<HomeTripCardsModel>('homeTripCards').valueChanges();
+  public getTargetUserTrips(targetUser): Subscription {
+    return this.db.collection('users').doc(targetUser).collection<HomeTripCardsModel>('createdTrips').get()
+        .subscribe(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            this.allHomeTripCards.push(doc.data());
+          });
+        });
   }
+
 }
