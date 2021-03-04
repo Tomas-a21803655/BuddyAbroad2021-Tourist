f5012f37ac586fc6c4271ca807c216d8fde488cc
Tomas-a21803655 <57908569+Tomas-a21803655@users.noreply.github.com>
2021-03-04 23:22:59 +0000

    visit details now has buddy info

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -35,7 +35,6 @@ export class ProfilePage implements OnInit {
       this.userRating = data.rating;
       this.userImage = data.image;
     });
-
   }
 
 }
--- src/app/visit-details/visit-details.page.ts
+++ src/app/visit-details/visit-details.page.ts
@@ -1,49 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NavController} from '@ionic/angular';
 import {ActivatedRoute, Router} from '@angular/router';
-import { FireStorageService } from '../fire-storage.service';
+import {FireStorageService} from '../fire-storage.service';
 import {HomeTripCardsModel} from '../shared/homeTripCards.model';
 import {AngularFirestore} from '@angular/fire/firestore';
-import {Observable} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 
 @Component({
-  selector: 'app-visit-details',
-  templateUrl: './visit-details.page.html',
-  styleUrls: ['./visit-details.page.scss'],
+    selector: 'app-visit-details',
+    templateUrl: './visit-details.page.html',
+    styleUrls: ['./visit-details.page.scss'],
 })
 export class VisitDetailsPage implements OnInit {
 
-  public trips: Observable<HomeTripCardsModel>;
-  public trip: HomeTripCardsModel;
+    public trips: Observable<HomeTripCardsModel>;
+    public trip: HomeTripCardsModel;
+    public userName;
+    public userDescription;
+    public userRating;
+    public userImage;
 
 
-  constructor(private router: Router,private navCtrl:NavController,
-              public fireStorageService: FireStorageService,
-              private route: ActivatedRoute,public db: AngularFirestore) { }
+    constructor(private router: Router, private navCtrl: NavController,
+                public fireStorageService: FireStorageService,
+                private route: ActivatedRoute, public db: AngularFirestore) {
+    }
 
-  ngOnInit() {
-    const tripId: string = this.route.snapshot.paramMap.get('id');
+    ngOnInit() {
+        const tripId: string = this.route.snapshot.paramMap.get('id');
 
-    this.db.collection('users').get()
-        .subscribe(querySnapshot => {
-          querySnapshot.forEach(doc => {
-            this.trips = this.fireStorageService.getTripDetail(tripId,doc.id);
-            this.trips.forEach( (element: HomeTripCardsModel) => {
-              if (element?.id === tripId){
-                this.trip = element;
-              }
+        this.db.collection('users').get()
+            .subscribe(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                    this.trips = this.fireStorageService.getTripDetail(tripId, doc.id);
+                    this.trips.forEach((element: HomeTripCardsModel) => {
+                        if (element?.id === tripId) {
+                            this.trip = element;
+                            // after here
+                            this.fireStorageService.getUserDocInfo().subscribe((data) => {
+                                this.userName = data.name;
+                                this.userDescription = data.description;
+                                this.userRating = data.rating;
+                                this.userImage = data.image;
+                            });
+                        }
+                    });
+                });
             });
+    }
 
-          });
-        });
-  }
+    goback() {
+        this.navCtrl.pop();
+    }
 
-  goback() {
-    this.navCtrl.pop();
-  }
-
-  public openPlanVisitPage(): void {
-    this.router.navigate(['/plan-visit']);
-  }
+    public openPlanVisitPage(): void {
+        this.router.navigate(['/plan-visit']);
+    }
 
 }
