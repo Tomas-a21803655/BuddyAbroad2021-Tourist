69d32eb1ce870d93d96934dbe4e99e24e746f30f
Tomas-a21803655 <57908569+Tomas-a21803655@users.noreply.github.com>
2021-03-05 22:47:31 +0000

    displays correct info, data time quikyness

--- src/app/buy-visit/buy-visit.page.ts
+++ src/app/buy-visit/buy-visit.page.ts
@@ -1,22 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NavController} from '@ionic/angular';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import {HomeTripCardsModel} from '../shared/homeTripCards.model';
+import {FireStorageService} from '../fire-storage.service';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {Observable} from 'rxjs';
+import firebase from 'firebase';
 
 @Component({
-  selector: 'app-buy-visit',
-  templateUrl: './buy-visit.page.html',
-  styleUrls: ['./buy-visit.page.scss'],
+    selector: 'app-buy-visit',
+    templateUrl: './buy-visit.page.html',
+    styleUrls: ['./buy-visit.page.scss'],
 })
 export class BuyVisitPage implements OnInit {
 
-  constructor(private navCtrl:NavController,private route: ActivatedRoute,) { }
+    public trips: Observable<HomeTripCardsModel>;
+    public trip: HomeTripCardsModel;
+    public desiredDate;
+    public desiredTime;
+    public desiredParticipants;
+    public desiredLanguage;
 
-  ngOnInit() {
-    const tripId: string = this.route.snapshot.paramMap.get('id');
+    constructor(private router: Router, private navCtrl: NavController,
+                public fireStorageService: FireStorageService,
+                private route: ActivatedRoute, public db: AngularFirestore) {
+    }
 
-  }
+    ngOnInit() {
+        const tripId: string = this.route.snapshot.paramMap.get('id');
 
-  goback() {
-    this.navCtrl.pop();
-  }
+        this.db.collection('users').get()
+            .subscribe(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                    this.trips = this.fireStorageService.getTripDetail(tripId, doc.id);
+                    this.trips.forEach((element: HomeTripCardsModel) => {
+                        if (element?.id === tripId) {
+                            this.trip = element;
+                        }
+                    });
+                });
+            });
+        const currentUser = firebase.auth().currentUser;
+        this.db.collection('users').doc(currentUser.uid).collection('unverifiedTrip').doc('unverified').valueChanges()
+            .subscribe((data) => {
+                this.desiredDate = data.desiredDate.split('T',1);
+                this.desiredTime =  data.desiredTime.substring(11,16);
+                this.desiredParticipants = data.desiredParticipants;
+                this.desiredLanguage = data.desiredLanguage;
+            });
+    }
+
+    goback() {
+        this.navCtrl.pop();
+    }
 }
