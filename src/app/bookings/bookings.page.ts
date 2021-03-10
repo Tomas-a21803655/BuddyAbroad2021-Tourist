97fd5e011935fd5327347fe27966e1bc94c89298
Tomas-a21803655 <57908569+Tomas-a21803655@users.noreply.github.com>
2021-03-10 20:09:35 +0000

    booking screen tracks booked now

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,27 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AngularFirestore} from '@angular/fire/firestore';
-import {Observable} from 'rxjs';
 import {FireStorageService} from '../fire-storage.service';
-import {HomeTripCardsModel} from '../shared/homeTripCards.model';
 
 
 @Component({
-  selector: 'app-bookings',
-  templateUrl: './bookings.page.html',
-  styleUrls: ['./bookings.page.scss'],
+    selector: 'app-bookings',
+    templateUrl: './bookings.page.html',
+    styleUrls: ['./bookings.page.scss'],
 })
 export class BookingsPage implements OnInit {
-  public homeTripCards;
+    public homeTripCards;
 
-  constructor(private router: Router, public db: AngularFirestore,public fireStorageService: FireStorageService,) {
-  }
+    constructor(private router: Router, public db: AngularFirestore, public fireStorageService: FireStorageService,) {
+    }
 
-  ngOnInit() {
-    this.homeTripCards = this.fireStorageService.getUserBookedTrips();
-  }
-
-  getAllhomeTripCards(): Observable<any> {
-    return this.db.collection<any>('homeTripCards').valueChanges();
-  }
+    ngOnInit() {
+        this.homeTripCards = this.fireStorageService.getUserBookedTrips();
+    }
 }
