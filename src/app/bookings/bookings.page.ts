0923bee2c3eaf1cd8300a649d4c6327d4637c0c9
Tomas-a21803655 <57908569+Tomas-a21803655@users.noreply.github.com>
2021-03-20 23:33:39 +0000

    UI booked CHANGE,need to implement form numbers XX  x XX

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -11,11 +11,17 @@ import {FireStorageService} from '../fire-storage.service';
 })
 export class BookingsPage implements OnInit {
     public homeTripCards;
+    type: string;
 
     constructor(private router: Router, public db: AngularFirestore, public fireStorageService: FireStorageService,) {
     }
 
     ngOnInit() {
+        this.type = 'booked';
         this.homeTripCards = this.fireStorageService.getUserBookedTrips();
+    }
+
+    segmentChanged(ev: any) {
+        console.log('Segment changed', ev);
     }
 }
